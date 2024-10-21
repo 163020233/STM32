fff59cf2eade5dac9f7c5d8748dd25bb60f2613b
Lt <1497098056@qq.com>
2024-10-22 07:12:27 +0800

    [first]1.add task

---
+++ cmake-build-debug-mingw-arm/CMakeFiles/IGT6_CLION.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for IGT6_CLION.elf.
+# This may be replaced when dependencies are built.
---
+++ test/test.h
@@ -0,0 +1,11 @@
+//
+// Created by Administrator on 2024/10/22.
+//
+
+#ifndef IGT6_CLION_TEST_H
+#define IGT6_CLION_TEST_H
+
+void led(void);
+
+#endif // IGT6_CLION_TEST_H
+
